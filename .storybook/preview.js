21e172b9ae2f97bd859f65dc1a60ba9b7d37a9eb
ali <a.khadim@xpay.de>
2021-04-09 16:32:31 +0200

    Add Global wrapper component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import React from "react"
+
+export const decorators = [
+  (Story) => (
+    <div style={{ margin: "3em" }}>
+      <h2>Hello world!</h2>
+      <Story />
+    </div>
+  ),
+]
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
